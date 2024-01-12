8969f6518bf4491c75006e33fc41b5b93b03c165
anna200167 <hopperdevil200167@gmail.com>
2024-01-12 20:09:10 +0530

    anish

---
+++ BricksFunds/src/main/java/com/bricksfunds/BricksFundsApplication.java
@@ -0,0 +1,13 @@
+package com.bricksfunds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricksFundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricksFundsApplication.class, args);
+	}
+
+}
---
+++ BricksFunds/src/main/java/com/bricksfunds/repo/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.bricksfunds.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bricksfunds.entity.Role;
+
+
+
+public interface RoleRepository extends JpaRepository<Role,Long>{
+
+}
