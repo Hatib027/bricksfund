534ef086f466ba25a724d8a715123596aeb9ff09
anna200167 <87202388+anna200167@users.noreply.github.com>
2024-01-16 12:30:43 +0530

    Update UpiWidthrawlRepository.java

--- src/main/java/com/bricksfunds/repo/UpiWidthrawlRepository.java
+++ src/main/java/com/bricksfunds/repo/UpiWidthrawlRepository.java
@@ -12,7 +12,7 @@ public interface UpiWidthrawlRepository extends JpaRepository<UPIWithdrawl,Integ
     @Query("select p.id, p.userName, p.amount,p.userName, p.holderName,p.Date from UPIWithdrawl p where p.status = 0")
     public List<?> getAllUpiWidthrawlRequests();
     
-    public List<UPIWithdrawl> findByUserName(String username);
+    public List<UPIWithdrawl> findByUserNameOrderByIdDesc(String username);
     
     public  List<UPIWithdrawl> findByStatusOrderByIdDesc(int i);
 }
